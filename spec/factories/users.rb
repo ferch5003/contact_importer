19dca78c45663b3ab79ae216ac1b014a632e482b
Fernando Visbal <ferch5003@gmail.com>
2021-11-21 18:37:18 -0500

    test: Started model test on User

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:id) { |n| }
+    sequence(:email) { |n| "#{Faker::Internet.email}#{n}" }
+    password { Faker::Internet.password }
+    created_at { Time.now }
+    updated_at { Time.now }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+  end
+
+  describe 'scopes' do
+  end
+
+  describe 'class methods' do
+  end
+
+  describe 'instance methods' do
+  end
 end
