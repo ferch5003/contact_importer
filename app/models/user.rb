33ca65d76a3d536fda4dd1efa3b8791270182c83
Fernando Visbal <ferch5003@gmail.com>
2021-11-21 18:30:04 -0500

    feat: Added soft delete feat to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   include Gravtastic
 
+  acts_as_paranoid
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
