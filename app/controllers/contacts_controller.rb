ec60e506904f9c0faa67a5d807b8045c64aff198
Fernando Visbal <ferch5003@gmail.com>
2021-11-22 23:54:31 -0500

    test: Added ContactsController actions

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,6 +2,6 @@ class ContactsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @pagy, @contacts = pagy(Contact.all)
+    @pagy, @contacts = pagy(current_user.contacts.order(created_at: :desc))
   end
 end
